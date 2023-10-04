131b914973015984480923cb5834df6ebda635b9
xtremfree <alaoui.dvp@gmail.com>
2023-10-04 19:20:41 +0100

    change document

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,97 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          {/* Text content */}
+          <div>
+            <p>
+À propos de YT Thumbnail Downloader
+YT (You Thumbnail) Le téléchargeur de miniatures YouTube vous permet de télécharger des miniatures YouTube facilement et rapidement. C'est un outil en ligne, pas besoin d'installer une application, entièrement gratuit pour tout le monde.
+
+Qu'est-ce que la miniature YouTube?
+La vignette YouTube est l'image de couverture vidéo qui donne un aperçu de la vidéo. Il existe deux types de vignettes YouTube: générées automatiquement par YouTube et téléchargées par le créateur. Les miniatures téléchargées par les créateurs sont également appelées miniatures personnalisées. Quel que soit le type de miniature, son URL est masquée, vous ne pouvez pas trouver le lien de la miniature sur YouTube et vous ne pouvez pas l'enregistrer.
+
+Combien de vignettes contient une vidéo YouTube?
+Une vidéo YouTube peut avoir jusqu'à neuf miniatures, mais toutes les vidéos n'ont pas neuf miniatures. Il existe sept miniatures garanties:
+
+Arrière-plan du joueur 480x360
+Démarrer 120x90
+Milieu 120x90
+Fin 120x90
+de haute qualité 480x360
+Qualité moyenne 320x180
+Qualité normale 120x90
+De plus, si la vidéo est en haute définition, il y aura deux autres tailles de miniature.
+
+Définition standard 640x480
+Résolution maximale 1920x1080
+Mais les tailles les plus couramment utilisées sont de haute qualité, de qualité moyenne et pleine grandeur. Par conséquent, le téléchargeur de miniatures YT Youtube fournit uniquement des téléchargements dans ces trois tailles. Si vous avez besoin d'autres tailles, je vous dirai comment l'obtenir plus tard.
+
+Pourquoi télécharger des vignettes?
+Les miniatures de vidéos YouTube sont l'une des métadonnées les plus importantes d'une vidéo, et des créateurs attentifs concevront soigneusement la couverture vidéo. Cela a rendu la plupart des couvertures vidéo sur YouTube belles, drôles ou créatives. De nombreuses personnes souhaitent télécharger et enregistrer. En général, sur la base des objectifs suivants:
+
+Enregistrez la miniature YouTube comme fond d'écran.
+Partagez des miniatures sur votre blog ou vos réseaux sociaux.
+Téléchargez les miniatures vidéo comme source d'inspiration pour la couverture vidéo.
+Pourquoi avez-vous besoin d'un téléchargeur de vignettes?
+Comme nous l'avons dit précédemment, le lien de l'image miniature de Youtube n'est pas visible, ce qui rend difficile l'enregistrement de la couverture vidéo. Bien que certains outils puissent saisir des miniatures, aucun d'entre eux n'est aussi simple que le téléchargeur de miniatures Youtube.
+
+Où trouver les liens vidéo YouTube?
+En raison de nos téléchargements de téléchargeurs de miniatures via un lien vidéo, vous devez trouver le lien vidéo avant de télécharger. Ouvrez la vidéo que vous souhaitez télécharger dans le navigateur ou l'application Youtube, cliquez sur le bouton de partage sous la vidéo, le lien vidéo Youtube se trouve dans l'interface contextuelle.
+
+Fonctionnalités de téléchargement de miniatures YouTube:
+Le téléchargeur de miniatures YT est un bon compagnon de créateurs YouTube. Il présente principalement les caractéristiques suivantes:
+
+Extraire facilement des miniatures YouTube en ligne
+
+Le téléchargeur de miniatures YouTube fonctionne comme un extracteur, lorsque vous mettez des liens vidéo dans la zone de recherche, il extrait immédiatement les images miniatures en ligne, pas besoin de télécharger la vidéo en premier.
+
+Trouvez et affichez rapidement les vignettes HD
+
+Seules les miniatures de petite et basse résolution s'affichent sur la page d'accueil youtube, la page de chaîne et la page de résultats de recherche. Si vous voulez voir les vignettes HD, vous devez regarder dans le code source de la page. Mais avec le téléchargeur de vignettes YT, il vous suffit de connaître le lien vidéo pour afficher rapidement les vignettes HD.
+
+Obtenir des liens vers des images miniatures de vidéos YouTube
+
+Après avoir entré le lien vidéo dans le téléchargeur, vous obtiendrez immédiatement la miniature de la vidéo. Cliquez sur le lien de l'image pour voir l'URL miniature dans la barre d'adresse du navigateur.
+
+Enregistrez les vignettes sur votre appareil
+
+Après avoir saisi la miniature avec le lien vidéo, vous pouvez cliquer sur l'image pour télécharger et enregistrer la miniature.
+
+Comment utiliser le téléchargeur de miniatures YT?
+Il est facile de saisir une vignette YouTube en utilisant notre téléchargeur de vignettes.
+
+1. Copiez et collez l'URL de la vidéo dans le champ de recherche ci-dessus.
+
+2. Le téléchargeur de vignettes YT saisit automatiquement trois tailles différentes de vignettes.
+
+3. Sélectionnez une miniature à télécharger. Nous vous recommandons de télécharger des miniatures HD.
+
+Comment obtenir toutes les tailles de miniatures YouTube?
+Comme nous l'avons dit précédemment, une vidéo peut avoir jusqu'à 9 miniatures de différentes tailles, et maintenant je vais vous dire comment obtenir toutes les miniatures. Avant de commencer, vous devez comprendre ce qu'est un identifiant vidéo.
+
+L'ID de vidéo YouTube est un ID unique utilisé pour identifier la vidéo téléchargée sur YouTube. L'ID vidéo Youtube est utilisé pour créer une URL unique pour l'affichage des vidéos et peut être utilisé pour intégrer des vidéos Youtube sur n'importe quel site Web.
+
+L'identifiant vidéo se trouve généralement à la fin de l'URL de la vidéo, comme les deux liens vidéo suivants:
+
+https://www.youtube.com/watch?v=rI3KQXNDQjI
+Le caractère dy6Hj0Kk-CA est l'ID vidéo. Vous trouverez ci-dessous une liste des neuf liens d'image miniature d'une vidéo, remplacez l'identifiant vidéo par le vôtre pour obtenir les vignettes vidéo.
+
+480x360: https://i1.ytimg.com/vi/VIDEO-ID/0.jpg
+120x90: https://i1.ytimg.com/vi/VIDEO-ID/1.jpg
+120x90: https://i1.ytimg.com/vi/VIDEO-ID/2.jpg
+120x90: https://i1.ytimg.com/vi/VIDEO-ID/3.jpg
+480x360: https://i1.ytimg.com/vi/VIDEO-ID/hqdefault.jpg
+320x180: https://i1.ytimg.com/vi/VIDEO-ID/mqdefault.jpg
+120x90: https://i1.ytimg.com/vi/VIDEO-ID/default.jpg
+640x480: https://i1.ytimg.com/vi/VIDEO-ID/sddefault.jpg
+1920x1080: https://i1.ytimg.com/vi/VIDEO-ID/maxresdefault.jpg
+Vous constaterez qu'il n'est pas si facile d'obtenir des miniatures vidéo de cette manière, si vous n'avez pas besoin de la taille de miniature supplémentaire, veuillez utiliser notre téléchargeur de miniatures pour télécharger directement.
+
+Puis-je utiliser les miniatures téléchargées sur YouTube?
+Le droit d'auteur de la vignette appartient au créateur ou au concepteur. Sans l'autorisation du titulaire des droits d'auteur, ils peuvent vous signaler et poursuivre en justice pour l'utilisation du contenu de la vignette. Par conséquent, je ne recommande pas de copier les vignettes vidéo des autres. Mais il peut être utilisé comme une inspiration de conception pour créer vos vignettes.
+            </p>
+          </div>
         </body>
       </Html>
     );
